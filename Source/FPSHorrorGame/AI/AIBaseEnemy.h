47216853b89b144a70e557246405a959c08b447c
Necati Akpınar <necatiakpinar1997@gmail.com>
2023-02-04 23:03:42 +0300

    Enemy AI tree is implemented

---
+++ Source/FPSHorrorGame/AI/AIBaseEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIBaseEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class FPSHORRORGAME_API AAIBaseEnemy : public AAIController
+{
+	GENERATED_BODY()
+	
+};
